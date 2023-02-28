44361a7cd26273ef7375fc7b3ff9f1b103d4e3a2
ctibenkana <115616062+ctibenkana@users.noreply.github.com>
2023-02-28 17:41:28 +0000

    🚧 Added routes to the main app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Container from './Container';
 import './App.css';
+import routes from "./routes";
+import { useState, Suspense, useMemo, lazy } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 
 function App() {
+  
+const allRoutes = useMemo((allRoutes) =>
+routes.map((route) => {
+  if (route.route !== null) {
+    return <Route exact path={route.route} element={route.component} key={route.key} />;
+  }
+
+  return null;
+}), [routes]);
+
   return (
     <div style={{backgroundColor: '#f2f2f2'}}>
-      <Container />
+      <Routes>
+        {allRoutes}
+      </Routes>
     </div>
   );
 }
