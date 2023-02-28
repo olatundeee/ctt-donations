9f0b4eec2945cc12371540315a522f934adc1185
ctibenkana <115616062+ctibenkana@users.noreply.github.com>
2023-02-28 18:56:14 +0000

    🚧  Customized for mobile view

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,5 @@
-import { useState } from "react";
-import {
-  Card,
-  Button,
-  Form,
-  Input,
-  Typography,
-} from "antd";
+import { useCallback, useState } from "react";
+import { Card, Button, Form, Input, Typography, Row, Col } from "antd";
 const { Title } = Typography;
 
 export default function Login() {
@@ -31,40 +25,75 @@ export default function Login() {
       username: username,
       password: password,
     };
+    console.log("RequestLogin: ", RequestLogin);
     // TODO: Call the end-point for login;
   }
-  return (
-    <div style={{ margin: "auto", height: "100vh", width: "max-content" }}>
-      <Card>
-        <Title level={4}>Welcome back! Please login to access</Title>
-        <Form style={{ height: "200px" }}>
-          <Input
-            type="text"
-            name="username"
-            value={username}
-            label="Username"
-            onChange={onChange}
-            placeholder="username"
-          />
-          <Input
-            type="password"
-            value={password}
-            name="password"
-            label="Password"
-            onChange={onChange}
-            placeholder="password"
-            style={{ marginTop: "10px" }}
-          />
 
-          <Button
-            type="primary"
-            style={{ marginTop: "10px", width: "100%" }}
-            onClick={handleSave}
+  const disableBtn = useCallback(
+    () => password === "" || username === "" || password.length < 4,
+    [password, username]
+  );
+
+  return (
+    <div
+      style={{
+        margin: "auto",
+        height: "100vh",
+        width: "max-content",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Row>
+        <Col
+          xs={{ span: 24 }}
+          sm={{ span: 24 }}
+          md={{ span: 24 }}
+          lg={{ span: 18 }}
+          xl={{ span: 18 }}
+          xxl={{ span: 18 }}
+          style={{ margin: "3rem 0", padding: "2.5%" }}
+        >
+          <Card
+            style={{
+              margin: "auto",
+              borderRadius: "2em",
+              maxWidth: "100vw",
+              height: "max-content",
+            }}
           >
-            Login
-          </Button>
-        </Form>
-      </Card>
+            <Title level={4}>Welcome back! Please login to access</Title>
+            <Form>
+              <Input
+                type="text"
+                name="username"
+                value={username}
+                label="Username"
+                onChange={onChange}
+                placeholder="username"
+              />
+              <Input
+                type="password"
+                value={password}
+                name="password"
+                label="Password"
+                onChange={onChange}
+                placeholder="password"
+                style={{ marginTop: "10px" }}
+              />
+
+              <Button
+                type="primary"
+                disabled={disableBtn()}
+                style={{ marginTop: "10px", width: "100%" }}
+                onClick={handleSave}
+              >
+                Login
+              </Button>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
